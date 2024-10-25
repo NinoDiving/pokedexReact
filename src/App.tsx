e75698896248c9c2261f16ab7e184fa0ee2945d2
Nino Jautee <jauteenino@gmail.com>
2024-10-25 16:14:22 +0200

    Finish React Quest 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 import "./App.css";
 
 function App() {
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+
+	const handleClickNext = () => {
+		setPokemonIndex((prevIndex) =>
+			prevIndex === pokemonList.length - 1 ? 0 : prevIndex + 1,
+		);
+	};
+
+	const handleClickPrev = () => {
+		setPokemonIndex((prevIndex) =>
+			prevIndex === 0 ? pokemonList.length - 1 : prevIndex - 1,
+		);
+	};
+
 	return (
 		<>
 			<PokemonCard
-				name="Bulbasaur"
-				imgSrc="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+				name={pokemonList[pokemonIndex].name}
+				imgSrc={pokemonList[pokemonIndex].imgSrc}
 			/>
+			<button type="button" onClick={handleClickNext}>
+				Suivant
+			</button>
+			<button
+				type="button"
+				onClick={() => handleClickPrev(setPokemonIndex + 1)}
+			>
+				Précédent
+			</button>
 		</>
 	);
 }
